b9bd1a312c8fb9aafe50bf15ec31c9ebaf32052a
Mattin <mattin-najafi@hotmail.com>
2019-11-04 14:09:08 +0100

    Finished code + jsdoc

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ let inputAge
 let userAge
 let firstOption
 
+
+/**
+ * Starting the game by deleting the startbutton and creating input and submitbuttons.
+ */
 function gameStarter() {
     sayingHello.innerHTML = "Hej. Vad heter du?"
     document.getElementById("inputBox").style.display = "block"
@@ -25,6 +29,9 @@ function gameStarter() {
     submit.addEventListener("click", welcome)
 
 }
+ /**
+  * Welcoming the user by taking the value of the input field and write it out in the paragraph
+  */
 
 function welcome() {
     userName = inputElement.value
@@ -35,6 +42,11 @@ function welcome() {
 
     setTimeout(askingForAge, 4000)
 }
+
+/**
+ * Asking the users age and saving the input, creating inputfield and submitbox
+ */
+
 function askingForAge() {
     document.getElementById("btn").style.display = "None"
     sayingHello.innerHTML = "Hur gammal är du?"
@@ -49,6 +61,11 @@ function askingForAge() {
 
     submit.addEventListener("click", oldEnough)
 }
+
+/**
+ * Checking if the userinput is correct written. And using the value to change the paragraph
+ */
+
 function oldEnough() {
     userAge = inputAge.value
     age = Number(userAge)
@@ -65,11 +82,9 @@ function oldEnough() {
     }
 
 }
-
-function writeNumber() {
-    oldEnough()
-
-}
+/**
+ * Asking the user if it want to go through the gamerules, with a placeholder that shows the options
+ */
 
 function gameRules() {
     sayingHello.innerHTML = "Vill du gå igenom spelreglerna?"
@@ -90,6 +105,10 @@ function gameRules() {
     secondOption.addEventListener("click", checkValue)
 }
 
+/**
+ * Checking the input of the user and using if/else-statements to know which way the game should go
+ */
+
 function checkValue() {
     userAnswerYes = firstOption.value
 
@@ -107,22 +126,34 @@ function checkValue() {
         return;
     }
 }
-
+/**
+ * Writing out the gamerules
+ */
 function onlyRule() {
     sayingHello.innerHTML = "Svara på frågorna som dyker upp och vänta tålmodigt till nästa"
     document.getElementById("infoBox").style.display = "None"
     setTimeout(noRule, 4000)
 }
-
+/**
+ * Story starting
+ */
 function noRule() {
     sayingHello.innerHTML = "Okej, då kör vi.. "
     document.getElementById("infoBox").style.display = "None"
     setTimeout(firstStory, 4000)
 }
+
+/**
+ * changing the innerhtml of <p>,  and the story continues
+ */
 function firstStory() {
     sayingHello.innerHTML = "Du är ute på promenad i skogen och plockar svamp när.."
     setTimeout(screamingMan, 4000)
 }
+
+/**
+ * The murderer of the story shows up with a img, and story continues
+ */
 
 function screamingMan() {
     sayingHello.innerHTML = "EN MÖRDARE DYKER UPP! "
@@ -140,12 +171,16 @@ function manAskingQuestion() {
     sayingHello.innerHTML = "Han tittar på dig konstigt och ställer en fråga:"
     setTimeout(deathQuestion, 4000)
 }
-
+/**
+ * Question about life shows upp
+ */
 function deathQuestion() {
     sayingHello.innerHTML = "Vill du dö?"
     setTimeout(buttonPopUp, 3000)
 }
-
+/**
+ * Button shows up, yes or no, they take user to two different paths
+ */
 function buttonPopUp() {
     sayingHello.innerHTML = "Vill du dö?"
 
@@ -159,9 +194,10 @@ function buttonPopUp() {
 
     firstOption.style.background = "Red"
     secondOption.style.background = "Green"
-
-
     firstOption.addEventListener("click",
+    /**
+     * If user choose to die, the paragraph changes and it sends user to the end of the game with the function gunshot 
+     */
         function restInPeace() {
             sayingHello.innerHTML = "Han tar fram en pistol"
             document.getElementById("yesOrNo").style.display = "none"
@@ -172,6 +208,9 @@ function buttonPopUp() {
 
 }
 
+/**
+ * User get shot with some style attributes and it makes display none on the buttons
+ */
 function gunshot() {
     document.getElementById("yesOrNo").style.display = "none"
     alert("BOM! Han dödade dig")
@@ -180,7 +219,9 @@ function gunshot() {
     document.getElementById('infoBox').style.display = "none"
     setTimeout(theEnd, 1500)
 }
-
+/**
+ * Game finished and img of a dead guy shows up
+ */
 function theEnd() {
     document.getElementById("imgBox").style.display = "Block"
     document.body.style.background = "rgb(247, 247, 247)"
@@ -189,7 +230,9 @@ function theEnd() {
     var src = document.getElementById('imgBox')
     src.append(img)
 }
-
+/**
+ * If the user doesnt want to die, it comes here and paragraph shows up and the story continues
+ */
 function gameContinues() {
     document.getElementById("yesOrNo").style.display = "none"
     sayingHello.style.display = "block"
@@ -197,12 +240,17 @@ function gameContinues() {
     sayingHello.innerHTML = "Mannen fortsätter: Tur det! För att jag ska låta dig och dina svampar vara ifred.."
     setTimeout(beforeLastQuestion, 4000)
 }
-
+/**
+ * Question of the game asks and user have to follow the instructions coming
+ */
 function beforeLastQuestion() {
     sayingHello.innerHTML = "Så måste du gissa vilket nummer jag tänker på mellan 1-100. Du har 10 chanser på dig.. Annars dödar jag dig"
     setTimeout(scaryLaugh, 4000)
 }
 
+/**
+ * Scary laugh sound coming
+ */
 function scaryLaugh() {
     var laugh = document.getElementById("myAudio")
     laugh.style.display = "block"
@@ -215,6 +263,11 @@ let nrOfGuesses = 0
 let guessNumber
 let inputGuess
 let randomNumber = Math.ceil(Math.random() * 100)
+
+/**
+ * creating inputfield and submitbox for the users guesses
+ */
+
 function guesses() {
 
     sayingHello.innerHTML = "Mannen tittar med ett konstigt leende och säger: Vilket nummer vill du gissa på?"
@@ -227,8 +280,11 @@ function guesses() {
     document.getElementById("submitBox2").appendChild(submitGuess)
 
     submitGuess.addEventListener("click", checkGuesses)
-
 }
+/**
+ * Saving the input, checking the value of the input and see what it is and 
+ * depending of the answer different options happens
+ */
 function checkGuesses() {
     inputGuess = guessNumber.value
     nrOfGuesses++
@@ -253,26 +309,34 @@ function checkGuesses() {
         sayingHello.innerHTML = "Försök igen.. hahahaha. Din gissning är högre än numret jag tänker på"
     }
 }
-
+/**
+ * if all guesses is wrong then game over, display none on the button and inputfield 
+ */
 function gameOver() {
     sayingHello.innerHTML = "Ha Ha Ha, jag varnade dig. säger mannen samtidigt som han tar upp en pistol"
     document.getElementById("submitBox2").style.display = "none"
     document.getElementById("inputBox2").style.display = "none"
     setTimeout(gunshot, 3000)
 }
-
+/**
+ * if the guess was correct, hiding the input and submitbox and sends me to next function
+ */
 function happyEnding() {
     sayingHello.innerHTML = "Nämen, det är din turdag, du gissade rätt, säger mannen"
     document.getElementById("submitBox2").style.display = "none"
     document.getElementById("inputBox2").style.display = "none"
     setTimeout(endOfGame, 3000)
 }
-
+/**
+ * End of story, last paragraph change of the game
+ */
 function endOfGame() {
     sayingHello.innerHTML = "Grattis. Ska inte störa dig mer, fortsätt på din svamprunda"
     setTimeout(imgOfMushroom, 3000)
 }
-
+/**
+ * Adding a image of a mushroom and display none on all the box
+ */
 function imgOfMushroom() {
     document.getElementById("paragraph").style.display = "none"
     document.getElementById("imgBox").style.display = "flex"
@@ -282,7 +346,9 @@ function imgOfMushroom() {
     src.append(mushroomImg)
     setTimeout(absoluteEndingOfGame, 2000)
 }
-
+/** 
+ *  Creating an element (h1) that is the ending of the game.
+ */
 function absoluteEndingOfGame() {
     let gameOver = document.createElement("h1")
     document.getElementById("imgBox").append(gameOver)
